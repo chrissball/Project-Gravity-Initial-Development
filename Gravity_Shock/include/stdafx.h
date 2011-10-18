f5183db5bc98068d04773c1e49ce5d221e726abd
Chris Ball <floobieg@live.co.uk>
2011-10-18 11:44:31 +0100

    Commit the project so far

---
+++ Gravity_Shock/include/stdafx.h
@@ -0,0 +1,37 @@
+#include "windows.h"
+
+#include "ExampleApplication.h"
+#include "../res/resource.h"
+#include "OgreBulletDynamicsRigidBody.h"				 // for OgreBullet
+#include "Shapes/OgreBulletCollisionsStaticPlaneShape.h" // for static planes
+#include "Shapes/OgreBulletCollisionsBoxShape.h"		 // for Boxes
+
+#include <OgreCamera.h>
+#include <OgreEntity.h>
+#include <OgreLogManager.h>
+#include <OgreRoot.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreConfigFile.h>
+#include <OgreWindowEventUtilities.h>
+
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+
+#include <SdkTrays.h>
+#include <SdkCameraMan.h>
+
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+
+#include <CEGUI.h>
+#include <RendererModules/Ogre/CEGUIOgreRenderer.h>
+
+#include "Hydrax/Hydrax.h"
+#include "Hydrax/Noise/Perlin/Perlin.h"
+#include "Hydrax/Modules/ProjectedGrid/ProjectedGrid.h"
+
+#include "Caelum.h"
---
+++ Gravity_Shock/src/Main.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "windows.h"
+#include "Gravity_Shock.h"
+
+#define WIN32_LEAN_AND_MEAN
+
+// For the finished release version
+/*INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+{
+ 	// Create application object
+ 	Gravity_Shock app;
+ 
+ 	try {
+ 		app.go();
+ 	} catch( Ogre::Exception& e ) {
+ 		MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+ 	}
+ 
+ 	return 0;
+}*/
+
+// For the console debug version
+int main( int argc, const char* argv[] )
+{
+ 	// Create application object
+ 	Gravity_Shock app;
+ 
+ 	try {
+ 		app.go();
+ 	} catch( Ogre::Exception& e ) {
+ 		MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+ 	}
+ 
+ 	return 0;
+}
